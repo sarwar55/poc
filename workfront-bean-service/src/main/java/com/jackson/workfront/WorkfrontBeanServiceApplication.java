a1f20b5a01be5685baf19bb91d023b460fee0c37
A S M SARWAR ZAHAN <a.s.m.sarwar.zahan@gmail.com>
2018-04-09 22:08:04 -0400

    Initial commit
    Initial spring boot application commit

---
+++ workfront-bean-service/src/main/java/com/jackson/workfront/WorkfrontBeanServiceApplication.java
@@ -0,0 +1,12 @@
+package com.jackson.workfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkfrontBeanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkfrontBeanServiceApplication.class, args);
+	}
+}
